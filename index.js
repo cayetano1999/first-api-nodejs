882292a1cc5a1d6eefa93adae373b2031a3493d2
CayetanoMarquez <josuecayetano@hotmail.com>
2020-05-10 17:35:45 -0400

    add cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = require('./app.js');
 var mongoose = require('mongoose');
 var port = 6874;
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/portafolio')
+mongoose.connect('mongodb://localhost:27017/portafolio', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
     console.log('Connetion has been ready');
     //Crecion del server
